df399a9dda577cc9734d782fe312c3d53241e6a0
yoshi-automation <yoshi-automation@google.com>
2019-06-04 12:28:51 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- src/v1beta1/container_analysis_v1_beta1_client.js
+++ src/v1beta1/container_analysis_v1_beta1_client.js
@@ -69,12 +69,15 @@ class ContainerAnalysisV1Beta1Client {
   constructor(opts) {
     this._descriptors = {};
 
+    const servicePath =
+      opts.servicePath || opts.apiEndpoint || this.constructor.servicePath;
+
     // Ensure that options include the service address and port.
     opts = Object.assign(
       {
         clientConfig: {},
         port: this.constructor.port,
-        servicePath: this.constructor.servicePath,
+        servicePath,
       },
       opts
     );
@@ -179,6 +182,14 @@ class ContainerAnalysisV1Beta1Client {
    * The DNS address for this API service.
    */
   static get servicePath() {
+    return 'containeranalysis.googleapis.com';
+  }
+
+  /**
+   * The DNS address for this API service - same as servicePath(),
+   * exists for compatibility reasons.
+   */
+  static get apiEndpoint() {
     return 'containeranalysis.googleapis.com';
   }
 
